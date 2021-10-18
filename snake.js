1419ab3a34c019c5916523eb5f64a2340eeb2745
jamewo6173 <92017205+jamewo6173@users.noreply.github.com>
2021-10-18 16:23:23 +0530

    Base Files Added

---
+++ snake.js
@@ -0,0 +1,68 @@
+
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+//create the unit
+const box = 32;
+
+//load images
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+// load audio files
+
+let dead = new Audio();
+let eat = new Audio();
+let up = new Audio();
+let right = new Audio();
+let left = new Audio();
+let down = new Audio();
+
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src = "audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
+
+// to prevent the scrolling during up and down keys were pressed
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+//create the snake
+
+let snake = [];
+
+snake[0] = {
+    x : 9 * box,
+    y : 10 * box
+};
+
+//create the food
+
+let food = {
+    x : Math.floor(Math.random()*17+1) * box,
+    y : Math.floor(Math.random()*15+3) * box
+}
+
+//create the score var
+
+let score = 0;
+
+// control the snake
+let d;
+
+document.addEventListener("keydown",direction);
+
+
+
+
+let game = setInterval(draw,100);
